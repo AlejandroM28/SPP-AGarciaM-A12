042cbd4e84c7493d15ad1b7d91884833a1ed50b0
alejandro monroy <alejandromonroy@la-air-de-lisdy.lan>
2017-04-18 23:12:28 -0500

    Actividad 12

---
+++ src/spp/agarciam/a12/SPPAGarciaMA12.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp.agarciam.a12;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+/**
+ *
+ * @author alejandro garcía monroy
+ */
+public class SPPAGarciaMA12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double estatura = solicitaDouble("Introduzca su estatura (m)");
+        double peso = solicitaDouble("Introduzca su peso (kg)");
+        
+        double imc = peso / Math.pow(estatura,2);
+        
+        JOptionPane.showMessageDialog (null, "Su IMC es igual a: " + imc);
+        
+        if (imc > 25){
+            JOptionPane.showMessageDialog (null, "Estás en sobrepeso");
+        }else if (imc < 18){
+            JOptionPane.showMessageDialog (null, "Estás en desnutrición");
+        }else {
+            JOptionPane.showMessageDialog (null, "Estás en peso normal");
+        }
+        
+    }
+    public static double solicitaDouble(String mensaje){
+        Scanner kb = new Scanner (System.in);
+        String dato;
+        double num = 0;
+        boolean flag;
+        do{
+            try{
+            dato = JOptionPane.showInputDialog (null, mensaje);
+            num = Double.parseDouble(dato);
+            flag=false;
+            }catch (Exception ex) {
+                JOptionPane.showMessageDialog (null, "Error");
+                flag=true;
+                } 
+        } while (flag); 
+        return num;
+    }
+}
+    
+    
+}
